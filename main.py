5747e1c48f991d16bce71bb7b3a9c04a72f6fdc3
Lucas Gonella <goonella@hotmail.com>
2026-07-21 12:20:19 -0300

    feat: adiciona versão ao health check

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ app = FastAPI(title="DevOps SRE Lab")
 def health_check():                                     # define a função executada
     return {                                            # conteúdo da resposta
         "status": "healthy",
-        "service": "devops-sre-lab"
+        "service": "devops-sre-lab",
+        "version": "1.0.0"
     }
